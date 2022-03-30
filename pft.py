aa3141cd69ba61f0b3c521b128d4a062a8cec58b
Tyson Wasylik <tyson.wasylik1@ucalgary.ca>
2022-03-30 13:33:17 -0600

    ;)

--- pft.py
+++ pft.py
@@ -16,22 +16,33 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         
+        
+        #exercise list
         exercises = sql.get_exercises()
         self.ex_nav = gui.Exercise_Nav(self)
         self.ex_nav.populate_list(exercises)
         self.rowconfigure(0,weight=1)
         self.columnconfigure(0,weight=1)
-        self.ex_nav.grid(row=0,column=0,sticky='NS')
+        self.ex_nav.grid(row=0,column=0,sticky='NSEW')
         
+        
+        #report type
         self.rep_type = gui.Report_Type(self)
-        self.rep_type.grid(row=0,column=1)
+        self.rep_type.grid(row=1,column=1)
         
-        self.cal = gui.Date_Chooser(self)
-        self.cal.grid(row=0,column=0,sticky='W')
+        
+        
+        
+        #calendar
+        self.cal_start = gui.Date_Chooser(self)
+        self.cal_start.grid(row=0,column=0,sticky='W')
+        self.cal_start.frame.grid(row=3,column = 0)
+        self.cal_start.date_label.config(text="Start Date")
 
-        
-        
-        
+        self.cal_end = gui.Date_Chooser(self)
+        self.cal_end.grid(row=0,column=1,sticky='NEW')
+        self.cal_end.frame.grid(row=3,column = 1)
+        self.cal_end.date_label.config(text="End Date")
         
 
 def main():
--- pft_gui.py
+++ pft_gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkcalendar import Calendar
 import datetime
+
+
 class Exercise_Nav(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
@@ -9,26 +11,24 @@ class Exercise_Nav(tk.Frame):
         
         self.rowconfigure(0,weight = 1)
         self.columnconfigure(0,weight=1)
-             
+        
         #frame for listbox and scroll
-        frame = tk.Frame()
-        frame.rowconfigure(0,weight=1)
-        frame.columnconfigure(0,weight=1)
+        self.frame = tk.Frame(highlightbackground="black", highlightthickness=2)
+        self.frame.rowconfigure(0,weight=1)
+        self.frame.columnconfigure(0,weight=1)
         
         #set up elements
-        label_title = tk.Label(text = "Exercises")
-        self.scrollbar = tk.Scrollbar(frame, orient='vertical',troughcolor="#abab33")
-        self.ex_box = tk.Listbox(frame,width = 35,height = 30, yscrollcommand=self.scrollbar.set)
+        label_title = tk.Label(self.frame,text = "Exercises")
+        self.scrollbar = tk.Scrollbar(self.frame, orient='vertical',troughcolor="#abab33")
+        self.ex_box = tk.Listbox(self.frame,width = 38,height = 15, yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.ex_box.yview)
  
         #place in grid
-        self.scrollbar.grid(row=0,column=1, sticky='NS')
-        self.ex_box.grid(row=0,column=0,sticky ='NS')
+        self.scrollbar.grid(row=1,column=1, sticky='NS')
+        self.ex_box.grid(row=1,column=0,sticky ='NS')
         label_title.grid(row=0,column=0)
-        frame.grid(row=1,column=0)
-
-
-        
+        self.frame.grid(row=1,column=0)
+   
     def populate_list(self,items):
         for el in items:
             self.ex_box.insert(el[0],el[1])
@@ -43,44 +43,58 @@ class Report_Type(tk.Frame):
         self.rowconfigure(0,weight = 1)
         self.columnconfigure(0,weight=1) 
         
+        
         #frame for RadioButtons and scroll
-        frame = tk.Frame()
-        frame.rowconfigure(0,weight=1)
-        frame.columnconfigure(0,weight=1)
+        self.frame = tk.Frame(highlightbackground="black", highlightthickness=2)
+        self.frame.rowconfigure(0,weight=1)
+        self.frame.columnconfigure(0,weight=1)
+        rep_label = tk.Label(self.frame,text="Report Type")
+        rep_label.grid(row=0,column = 0,sticky = 'NW')
         
+        
+        # Radio Buttons for report type
         self.v = tk.IntVar()
+        self.one_plus = tk.Radiobutton(self.frame, text="1+ Set Report", variable = self.v, value = 1)
+        self.weight =  tk.Radiobutton(self.frame, text="Bodyweight Report",variable = self.v, value = 2)
+        self.one_plus.grid(row=1,column=0,sticky='NW')
+        self.weight.grid(row=2,column=0,sticky='NW')
+        #frame.grid(row=1,column=1,sticky='NW')
+        self.frame.grid(row=1,column=1,sticky='NEW')
         
-        self.one_plus = tk.Radiobutton(frame, text="1+ Set Report", variable = self.v, value = 1)
-        self.weight =  tk.Radiobutton(frame, text="Bodyweight Report",variable = self.v, value = 2)
-        self.one_plus.grid(row=0,column=0,sticky='NW')
-        self.weight.grid(row=1,column=0,sticky='NW')
-        frame.grid(row=1,column=1,sticky='NW')
-        
-
 class Date_Chooser(tk.Frame):
+
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         
         
+        
+        self.frame = tk.Frame(highlightbackground="black", highlightthickness=2)
+        self.frame.rowconfigure(0,weight=1)
+        self.frame.columnconfigure(0,weight=1)
+        
+        self.cal = Calendar(self.frame, selectmode='day',year = 2020, month = 5, day = 22)
+        self.cal.grid(row = 1, column=0)
+        
+        
         self.rowconfigure(0,weight = 1)
         self.columnconfigure(0,weight=1)
         
-        frame = tk.Frame()
-        frame.rowconfigure(0,weight=1)
-        frame.columnconfigure(0,weight=1)
+        self.date_label = tk.Label(self.frame,text = "Default")
+        #self.end_label = tk.Label(self.frame, text = "End Date")
         
-        
-        self.cal = Calendar(frame, selectmode='day',year = 2020, month = 5, day = 22)
-        
-        #self.cal = Calendar(frame, selectmode='day',year = datetime.date.today().year, month = datetime.date.today().month, day = datetime.date.today().day)
-        self.cal.grid(row = 0, column=0)
-        frame.grid(row = 2, column = 0)
+        self.date_label.grid(row = 0, column = 0)
+        #self.end_label.grid(row = 0, column = 1)
+
+
+
+
+
+
 
 
 def ButtonGUI():
     screen = tk.Tk()
-
 
     #screen size
     screen.geometry('300x300')
